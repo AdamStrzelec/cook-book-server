a4811a6a191e3988cdff65c47b89de247e223d01
adastr96 <adam19960@op.pl>
2020-12-22 16:40:30 +0100

    server done

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+const recipeRoutes = require('./routes/recipe');
+const rateRoutes = require('./routes/rate');
+
+require('dotenv').config()
+
+const uri = 'mongodb://Adam_96:Adam!2#4@node-rest-shop-shard-00-00-klt2b.mongodb.net:27017,node-rest-shop-shard-00-01-klt2b.mongodb.net:27017,node-rest-shop-shard-00-02-klt2b.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// app.use('/', (req, res, next) => {
+//     return res.status(200).json({
+//         message: 'hello'
+//     })
+// })
+app.use('/uploads', express.static('uploads'))
+app.use('/user', userRoutes);
+app.use('/recipe', recipeRoutes);
+app.use('/rate', rateRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
